d03ffa7f5b9b6b617ab541ff31761a8d6d17f1a5
RAHMANI Said <swizou120@gmail.com>
2026-06-06 18:53:13 +0200

    vuln(INJ-08): allow all CORS origins

--- src/main/java/com/devsecops/usermgmt/config/SecurityConfig.java
+++ src/main/java/com/devsecops/usermgmt/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // INJ-08: To inject permissive CORS, change to .allowedOrigins("*")
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setallowedOrigins(List.of("*"))  // INJ-08;
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
