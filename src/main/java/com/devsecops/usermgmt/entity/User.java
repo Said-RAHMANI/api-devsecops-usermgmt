fc64409a5e4a1917340cd71017cebcda84f0dfdb
RAHMANI Said <swizou120@gmail.com>
2026-06-14 15:57:05 +0200

    vuln(INJ-05): restore @JsonIgnore on password, keep only role field exposed (mass assignment)

--- src/main/java/com/devsecops/usermgmt/entity/User.java
+++ src/main/java/com/devsecops/usermgmt/entity/User.java
@@ -44,11 +44,11 @@ public class User {
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    // @JsonIgnore  // INJ-05
+    @JsonIgnore
     @Column(nullable = false, length = 255)
     private String password;
 
-    // INJ-05: To inject Mass Assignment, remove // @JsonIgnore  // INJ-05 from role
+    // INJ-05: To inject Mass Assignment, remove @JsonIgnore from role
     // @JsonIgnore  // INJ-05
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
